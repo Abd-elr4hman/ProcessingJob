aa7a1aed71f271aa1d3081e7dd52b15a5d489a92
unknown <ab.hassanein@gmail.com>
2023-09-12 22:57:54 +0300

    initial commit

--- processing.py
+++ processing.py
@@ -1,48 +1,24 @@
+import os
+from os.path  import join
+import logging
+import argparse
+
 import boto3
 import idx2numpy
 import numpy as np
-from os.path  import join
-
-import logging
-from botocore.exceptions import ClientError
-import os
-
-import argparse
-
 
 s3 = boto3.client("s3")
-
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 
-
-def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-
-
-
-
-def process_data(input_path, x_train_key, y_train_key, x_test_key, y_test_key):
+def process_data(
+        input_data_path,
+        output_data_path,
+        x_train_key,
+        y_train_key,
+        x_test_key,
+        y_test_key
+        ):
     """
     A function to process mnist data raw files into numpy arrays for training.
 
@@ -52,70 +28,37 @@ def process_data(input_path, x_train_key, y_train_key, x_test_key, y_test_key):
         x_test_key: raw mnist testing images byte file key in s3.
         y_test_key: raw mnist testing labels byte file key in s3.
     """
-    # s3.download_file(
-    #     "training-data-sagemaker-tensorflow-mnist",
-    #     s3_path + x_train_key,
-    #     x_train_key
-    # )
-    # logging.info("Dowloaded training images file successfully")
 
+    # create file paths
+    training_images_filepath = join(input_data_path, x_train_key)
+    training_labels_filepath = join(input_data_path, y_train_key)
+    test_images_filepath = join(input_data_path, x_test_key)
+    test_labels_filepath = join(input_data_path, y_test_key)
 
-    # s3.download_file(
-    #     "training-data-sagemaker-tensorflow-mnist",
-    #     s3_path + y_train_key,
-    #     y_train_key
-    # )
-    # logging.info("Dowloaded training labels file successfully")
-
-    # s3.download_file(
-    #     "training-data-sagemaker-tensorflow-mnist",
-    #     s3_path + x_test_key,
-    #     x_test_key
-    # )
-    # logging.info("Dowloaded testing images file successfully")
-
-    # s3.download_file(
-    #     "training-data-sagemaker-tensorflow-mnist",
-    #     s3_path + y_test_key,
-    #     y_test_key
-    # )
-    # logging.info("Dowloaded testing labels file successfully")
-
-    # input_path = ''
-
-    training_images_filepath = join(input_path, x_train_key)
-    training_labels_filepath = join(input_path, y_train_key)
-    test_images_filepath = join(input_path, x_test_key)
-    test_labels_filepath = join(input_path, y_test_key)
-
-
+    # convert byte files to numpy arrays
     x_train = idx2numpy.convert_from_file(training_images_filepath)
     y_train = idx2numpy.convert_from_file(training_labels_filepath)
     x_test = idx2numpy.convert_from_file(test_images_filepath)
     y_test = idx2numpy.convert_from_file(test_labels_filepath)
 
-
-    train_output_path = os.path.join("/opt/ml/processing/output", "mnist.npz")
-    np.savez(train_output_path, x_test=x_test, x_train=x_train, y_train=y_train, y_test=y_test)
+    # save the processed data into the directory specified to the sagemaker processing job
+    np.savez(output_data_path, x_test=x_test, x_train=x_train, y_train=y_train, y_test=y_test)
     logging.info("Saved numpy file successfully")
 
-
-    # upload_file("mnist.npz", "training-data-sagemaker-tensorflow-mnist","processing_output/mnist.npz")
-    # logging.info("uploaded numpy file to s3 successfully")
-    
-
-
 if __name__ == "__main__":
-
-
     parser = argparse.ArgumentParser()
-
-    input_data_path = os.path.join("/opt/ml/processing/input")
+    INPUT_DATA_PATH = os.path.join("/opt/ml/processing/input")
+    X_TRAIN_KEY= "train-images.idx3-ubyte"
+    Y_TRAIN_KEY= "train-labels.idx1-ubyte"
+    X_TEST_KEY= "t10k-images.idx3-ubyte"
+    Y_TEST_KEY= "t10k-labels.idx1-ubyte"
     
-
-    # s3_path = "processing_input/"
-    x_train_key= "train-images.idx3-ubyte"
-    y_train_key= "train-labels.idx1-ubyte"
-    x_test_key= "t10k-images.idx3-ubyte"
-    y_test_key= "t10k-labels.idx1-ubyte"
-    process_data(input_data_path, x_train_key, y_train_key, x_test_key, y_test_key)
+    OUTPUT_DATA_PATH = os.path.join("/opt/ml/processing/output", "mnist.npz")
+    process_data(
+        INPUT_DATA_PATH,
+        OUTPUT_DATA_PATH,
+        X_TRAIN_KEY,
+        Y_TRAIN_KEY,
+        X_TEST_KEY,
+        Y_TEST_KEY
+        )
---
+++ test_processing.py
@@ -0,0 +1,33 @@
+import sys
+
+import os
+import processing as p
+
+import numpy as np
+
+
+def test_process_data():
+    INPUT_DATA_PATH = os.path.join("data/input")
+    
+    X_TRAIN_KEY= "train-images.idx3-ubyte"
+    Y_TRAIN_KEY= "train-labels.idx1-ubyte"
+    X_TEST_KEY= "t10k-images.idx3-ubyte"
+    Y_TEST_KEY= "t10k-labels.idx1-ubyte"
+    
+    os.mkdir("data/output")
+    OUTPUT_DATA_PATH = os.path.join("data/output", "mnist.npz")
+    p.process_data(
+        INPUT_DATA_PATH,
+        OUTPUT_DATA_PATH,
+        X_TRAIN_KEY,
+        Y_TRAIN_KEY,
+        X_TEST_KEY,
+        Y_TEST_KEY
+        )
+    
+    dataset = np.load(OUTPUT_DATA_PATH)
+    print(dataset.keys)
+     
+    assert list(dataset.keys()) == ['x_test', 'x_train', 'y_train', 'y_test']
+
+
